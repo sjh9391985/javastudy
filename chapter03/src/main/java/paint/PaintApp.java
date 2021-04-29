89936c83d34e4a9d82c8ba846644ca7cfb169441
BIT <BIT@DESKTOP-DSRR2KB>
2021-04-29 11:27:41 +0900

    instanceod 연산자사용

--- chapter03/src/main/java/paint/PaintApp.java
+++ chapter03/src/main/java/paint/PaintApp.java
@@ -4,24 +4,23 @@ public class PaintApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Point point1 = new Point(10,20);	
+		Point point1 = new Point(10, 20);
 		draw(point1);
 
 		Point point2 = new Point(100, 200);
 //		point2.show();
 //		point1.show(false);
 		draw(point2);
-		
+
 		Point point3 = new ColorPoint(50, 100, "red");
 		// point3.show();
 		draw(point3);
-		
-		
+
 //		drawPoint(point3);
 //		
 		Rect rect = new Rect();
-		//drawRect(rect)
-		//drawShape(rect)
+		// drawRect(rect)
+		// drawShape(rect)
 		draw(rect);
 
 		Traiangle triangle = new Traiangle();
@@ -29,11 +28,29 @@ public class PaintApp {
 
 		Circle circle = new Circle();
 		draw(circle);
-		
+
 		draw(new GraphicText("hello~"));
+		
+		//instanceof text <= 이항연산자
+		System.out.println(circle instanceof Object);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Circle);
+		
+		//오류: class는 hierachy 상위와 하위만 instanceof 연산자를 사용할 수 있다.
+		//System.out.println(circle instanceof Rect);
+		//System.out.println(circle instanceof Traiangle);
+		
+		Shape s = new Circle();
+		System.out.println(s instanceof Object);
+		System.out.println(s instanceof Shape);
+		System.out.println(s instanceof Circle);
+		System.out.println(s instanceof Rect);
+		
+		//interface는 hierachy와 상관없이 instanceof 연산자를 사용할 수 있다.
+		System.out.println(s instanceof Drawable);
+		System.out.println(s instanceof Runnable);
 
 	}
-
 
 	public static void draw(Drawable drawable) {
 		drawable.draw();
